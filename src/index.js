53b68c2dcf42ae1ecb088f1ea60f874e1e1bd1ec
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-04-17 10:41:24 +0200

    db connected

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
+
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/api-rest-exercise', {
+    useNewUrlParser: true,
+})
 
 // settings
 app.set('port', process.env.PORT || 3000)
