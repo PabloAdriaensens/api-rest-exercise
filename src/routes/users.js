56911efbffb8db1034a7ef761adafc7ad15bff23
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-04-18 09:37:34 +0200

    routing problem fixed

--- src/routes/users.js
+++ src/routes/users.js
@@ -19,17 +19,19 @@ const {
 router.get('/', index)
 router.post('/', newUser)
 
+// Routes with fixed segments
+router.get('/typeMessages', getTypeMessages)
+router.get('/senderMessages', getSenderMessages)
+router.get('/unreadMessages', getUnreadMessages)
+router.get('/mostMessages', getMostMessages)
+router.get('/pepinoMessages', getPepinoMessages)
+
+// Routes with variable segment
 router.get('/:userId', getUser)
 router.put('/:userId', updateUser)
 router.delete('/:userId', deleteUser)
 
 router.post('/:userId/messages', newMessage)
 router.get('/:userId/messages', getMessages)
-
-router.get('/typeMessages', getTypeMessages)
-router.get('/senderMessages', getSenderMessages)
-router.get('/unreadMessages', getUnreadMessages)
-router.get('/mostMessages', getMostMessages)
-router.get('/pepinoMessages', getPepinoMessages)
 
 module.exports = router
